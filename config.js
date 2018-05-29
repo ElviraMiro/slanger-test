b688bae0780d417ee631b5f3afd6e6a13193f545
Elvira Myroshnychenko <karashistka@gmail.com>
2018-05-29 13:55:34 +0300

    change devel

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    JWT_URL: process.env['JWT_URL'] || 'https://account.devel.etorox.io/api/v1/sessions',
-    MEMBER_URL: process.env['MEMBER_URL'] || 'https://platform.devel.etorox.io/api/v2/members/me',
-    APP_ID: process.env['APP_ID'] || 'c96da32cd8e95621b0ae0a468385cc',
-    USERS_YAML: process.env['USERS_YAML'] || 'users.yaml',
+    JWT_URL: process.env['JWT_URL'] || 'https://auth.development.etorox.io/api/v1/sessions',
+    MEMBER_URL: process.env['MEMBER_URL'] || 'https://api.development.etorox.io/api/v2/members/me',
+    BARONG_USER: {
+        email: 'devs@etorox.io',
+        password: 'Qwerty123',
+        application_id: "1f4c7db067c066c274b58b40bac5d161894e6835dfb0b7656d63b4b7"
+    },
     PUSHER_APP: process.env['PUSHER_APP'] || "peatio",
+    PUSHER_CLIENT_WS_PORT: process.env['PUSHER_CLIENT_WS_PORT'] || '80',
     PUSHER_HOST: process.env['PUSHER_HOST'] || "api.slanger.development.etorox.io",
+    PUSHER_ENCRYPTED: process.env['PUSHER_ENCRYPTED'] || true,
     PUSHER_CLIENT_WSS_PORT: process.env['PUSHER_CLIENT_WSS_PORT'] || "443",
     PUSHER_APP_KEY: process.env['PUSHER_APP_KEY'] || "10895827e9ed350f54bc74b6eb023ac81538bdf745baaf1d93cde2ff"
 }
--- index.js
+++ index.js
@@ -5,57 +5,61 @@ const axios = require('axios')
 
 const config = require('./config')
 
-var users = yaml.safeLoad(fs.readFileSync('users.yaml', 'utf8'));
 
-//for (var i=0; i < users.length; i++) {
+axios({
+    method: "post",
+    url: config.JWT_URL,
+    data: config.BARONG_USER
+}).then(function(response) {
+    var jwt = response.data
+    console.log("get jwt")
+    console.log(jwt)
     axios({
-        method: "post",
-        url: config.JWT_URL,
-        data: {email: users[0].email, password: users[0].password, application_id: config.APP_ID}
+        method: "get",
+        url: config.MEMBER_URL,
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        }
     }).then(function(response) {
-        var jwt = response.data
-        console.log("get jwt")
-        console.log(jwt)
-        axios({
-            method: "get",
-            url: config.MEMBER_URL,
-            headers: {
-                Authorization: `Bearer ${jwt}`
-            }
-        }).then(function(response) {
-            var sn = response.data.sn,
-                private_channel = `private-${sn}`
-            console.log("get member")
-            console.log(response.data)
-            var pusher = new Pusher({
-                app_id: config.PUSHER_APP,
-                key: config.PUSHER_KEY,
-                wsHost: config.PUSHER_HOST,
-                wssPort: config.PUSHER_CLIENT_WSS_PORT,
-                auth: {
-                    headers: {Authorization: `Bearer ${jwt}`}
-                }
-            });
-            var channel_private = pusher.subscribe(private_channel);
-            channel_private.bind('accounts', data => {
-                console.log("get private ACCOUNTS event", data);
-            });
-            var channel_tickers = pusher.subscribe('market-global');
-            channel_tickers.bind('tickers', data=> {
-                console.log("get private TICKERS event", data)
-            })
-        }).catch(function(err) {
-            if (err.response) {
-                console.log("error when get member", err.response.data);
-            } else {
-                console.log("error when get member",err);
+        var sn = response.data.sn,
+            private_channel = `private-${sn}`
+        console.log("get member")
+        console.log(response.data)
+        var pusher = new Pusher({
+            key: config.PUSHER_KEY,
+            wsHost: config.PUSHER_HOST,
+            wssPort: config.PUSHER_CLIENT_WSS_PORT,
+            wsPort: config.PUSHER_CLIENT_WS_PORT,
+            encrypted: config.PUSHER_ENCRYPTED,
+            auth: {
+                headers: {Authorization: `Bearer ${jwt}`}
             }
         });
+        var channel_private = pusher.subscribe(private_channel);
+        channel_private.bind('accounts', data => {
+            console.log("get private ACCOUNTS event", data);
+        });
+        channel_private.bind('account', data => {
+            console.log("get private ACCOUNT event", data);
+        });
+        channel_private.bind('order', data => {
+            console.log("get private ORDER event", data);
+        });
+        var channel_tickers = pusher.subscribe('market-global');
+        channel_tickers.bind('tickers', data=> {
+            console.log("get private TICKERS event", data)
+        })
     }).catch(function(err) {
         if (err.response) {
-            console.log("Error when get JWT", err.response.data);
+            console.log("error when get member", err.response.data);
         } else {
-            console.log("Error when get JWT", err);
+            console.log("error when get member",err);
         }
     });
-//}
+}).catch(function(err) {
+    if (err.response) {
+        console.log("Error when get JWT", err.response.data);
+    } else {
+        console.log("Error when get JWT", err);
+    }
+});
