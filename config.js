abb2ce927d33980d777d3976af81ea946f902cd4
Elvira Myroshnychenko <karashistka@gmail.com>
2018-05-30 10:48:15 +0300

    change pusher client

--- config.js
+++ config.js
@@ -9,8 +9,10 @@ module.exports = {
     PUSHER_APP_KEY: process.env['PUSHER_APP_KEY'] || "10895827e9ed350f54bc74b6eb023ac81538bdf745baaf1d93cde2ff",
     PUSHER_HTTP_HOST: process.env['PUSHER_HTTP_HOST'] || "api.slanger.development.etorox.io",
     PUSHER_HTTPS_PORT: process.env['PUSHER_HTTPS_PORT'] || "443",
+    PUSHER_HTTP_PORT: process.env['PUSHER_HTTP_PORT'] || "80",
     PUSHER_WS_HOST: process.env['PUSHER_WS_HOST'] || "ws.slanger.development.etorox.io",
-    PUSHER_CLIENT_WSS_PORT: process.env['PUSHER_CLIENT_WSS_PORT'] || "443",
+    PUSHER_WS_PORT: process.env['PUSHER_WS_PORT'] || "80",
+    PUSHER_WSS_PORT: process.env['PUSHER_WSS_PORT'] || "443",
     PUSHER_ENCRYPTED: process.env['PUSHER_ENCRYPTED'] || true,
     PUSHER_AUTH_ENDPOINT: process.env['PUSHER_AUTH_ENDPOINT'] || 'https://api.development.etorox.io/api/v2/pusher/auth'
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Pusher = require('pusher-js')
+const Pusher = require('pusher-slanger-client')
 const axios = require('axios')
 
 const config = require('./config.js')
@@ -22,32 +22,45 @@ axios({
         var sn = response.data.sn,
             private_channel = `private-${sn}`
         console.log("get member")
-        console.log(response.data)
-        var pusher = new Pusher(config.PUSHER_APP_KEY, {
+        console.log(response.data);
+        var options = {
+            slanger: config.PUSHER_WS_HOST,
             wsHost: config.PUSHER_WS_HOST,
-            wssPort: config.PUSHER_CLIENT_WSS_PORT,
+            wsPort: config.PUSHER_WS_PORT,
+            wssPort: config.PUSHER_WSS_PORT,
             httpHost: config.PUSHER_HTTP_HOST,
+            httpPort: config.PUSHER_HTTP_PORT,
             httpsPort: config.PUSHER_HTTPS_PORT,
             encrypted: config.PUSHER_ENCRYPTED,
             authEndpoint: config.PUSHER_AUTH_ENDPOINT,
             auth: {
-                headers: {Authorization: `Bearer ${jwt}`}
+               headers: {'Authorization': 'Bearer '+jwt}
             }
-        });
-        var channel_private = pusher.subscribe(private_channel);
-        channel_private.bind('accounts', data => {
-            console.log("get private ACCOUNTS event", data);
-        });
-        channel_private.bind('account', data => {
-            console.log("get private ACCOUNT event", data);
-        });
-        channel_private.bind('order', data => {
-            console.log("get private ORDER event", data);
-        });
-        var channel_tickers = pusher.subscribe('market-global');
-        channel_tickers.bind('tickers', data=> {
-            console.log("get private TICKERS event", data)
+        };
+        var pusher = new Pusher(config.PUSHER_APP_KEY, options);
+        setInterval(()=>{
+            console.log(pusher.connection.state)
+        },1000)
+        pusher.connection.bind('error', (err) => console.log({err}, 'error'));
+        pusher.connection.bind('failed', (err) => console.log({err}, 'failed'));
+        pusher.connection.bind('connected', function() {
+        
+            var channel_private = pusher.subscribe(private_channel);
+            channel_private.bind('accounts', data => {
+                console.log("get private ACCOUNTS event", data);
+            });
+            channel_private.bind('account', data => {
+                console.log("get private ACCOUNT event", data);
+            });
+            channel_private.bind('order', data => {
+                console.log("get private ORDER event", data);
+            });
+            var channel_tickers = pusher.subscribe('market-global');
+            channel_tickers.bind('tickers', data=> {
+                console.log("get global TICKERS event", data)
+            })
         })
+        
     }).catch(function(err) {
         if (err.response) {
             console.log("error when get member", err.response.data);
